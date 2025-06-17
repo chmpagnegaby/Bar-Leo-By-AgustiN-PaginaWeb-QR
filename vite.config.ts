3e39e16de31ea71bd3a7f4cec3ab9fce86457c48
chmpage_gaby <hcgabriel7@gmail.com>
2025-06-17 23:20:06 +0200

    Actualizacion subida del proyecto

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Bar-Leo-By-AgustiN-PaginaWeb-QR/',
   server: {
     host: "::",
     port: 8080,
