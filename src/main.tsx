a1e5185b0ad87f8379517f1134417914a3bedf14
chmpage_gaby <hcgabriel7@gmail.com>
2025-06-17 23:33:31 +0200

    Actualizacion

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // ⬅️ Importa esto
 import App from './App.tsx';
 import './index.css';
 
@@ -10,6 +10,8 @@ if (!container) throw new Error('Failed to find the root element');
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/Bar-Leo-By-AgustiN-PaginaWeb-QR/"> {/* ⬅️ Esto es esencial */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
